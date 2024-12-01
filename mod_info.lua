b1637fa7281865b4535a3654a9aa95370523d66b
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2024-12-01 14:54:08 -0800

    Mod Lua version 14

--- mod_info.lua
+++ mod_info.lua
@@ -1,11 +1,11 @@
 name = "RoweysSACURebalance"
-version = 13
+version = 14
 copyright = "Rowey"
 description = "Mod to alow Balance Team to Easily test the changes due to them being split in to separate PRs"
 author = "Rowey"
 url = ""
 icon = ""
-uid = "SCU-v13"
+uid = "SCU-v14"
 exclusive = false
 ui_only = false
 conflicts = {}
